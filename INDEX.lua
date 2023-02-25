1bbb9702d414bb1377204a44455c253bd5d4b619
XPLOTR <126346113+XPLOTR@users.noreply.github.com>
2023-02-25 18:04:05 +0100

    Create INDEX.lua

---
+++ INDEX.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XPLOTR/DOORS/main/GUI.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XPLOTR/DOORS/main/Entity%20Spawner.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XPLOTR/DOORS/main/Ominious.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XPLOTR/DOORS/main/Project%20WD.lua'))()
